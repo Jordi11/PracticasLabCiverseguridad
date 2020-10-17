9f97f88759c14e6c132a1d164ab57cae37b5e151
Jordi11 <47021254+Jordi11@users.noreply.github.com>
2020-10-17 08:05:28 -0500

    Add files via upload

---
+++ Actividad 2.ps1
@@ -0,0 +1,15 @@
+﻿Clear -Host
+$STRContraseña = "Cthulu"
+$STRError = "Intente de nuevo"
+$STRUsuario = "H.P. Locovecraft"
+Do {
+$Guess = Read-Host "Ingrese la password de usuario:" + $STRUsuario 
+If ($Guess -eq $STRContraseña)
+{"Correcto"; $STRError = "n"}
+
+else{
+$STRError = Read-Host "incorrecto `n Quieres Voler a intentar? (Si/No)"
+}
+}
+while ($STRError -ne "No")
+"vuelva pronto" 
